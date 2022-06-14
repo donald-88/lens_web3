8476467bcd800a9a52f6768edf244a7f98fd8ca9
McDonald Namba <nambamcdonald@gmail.com>
2022-06-14 18:40:10 +0200

    displaying query data to screen

--- api.js
+++ api.js
@@ -7,85 +7,85 @@ export const client = createClient({
 })
 
 export const recommendedProfiles = `
-  query RecommendedProfiles {
-    recommendedProfiles {
-          id
-        name
-        bio
-        attributes {
-          displayType
-          traitType
-          key
+query RecommendedProfiles {
+recommendedProfiles {
+      id
+    name
+    bio
+    attributes {
+      displayType
+      traitType
+      key
+      value
+    }
+    metadata
+    isDefault
+    picture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+      __typename
+    }
+    handle
+    coverPicture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+      __typename
+    }
+    ownedBy
+    dispatcher {
+      address
+      canUseRelay
+    }
+    stats {
+      totalFollowers
+      totalFollowing
+      totalPosts
+      totalComments
+      totalMirrors
+      totalPublications
+      totalCollects
+    }
+    followModule {
+      ... on FeeFollowModuleSettings {
+        type
+        amount {
+          asset {
+            symbol
+            name
+            decimals
+            address
+          }
           value
         }
-        metadata
-        isDefault
-        picture {
-          ... on NftImage {
-            contractAddress
-            tokenId
-            uri
-            verified
-          }
-          ... on MediaSet {
-            original {
-              url
-              mimeType
-            }
-          }
-          __typename
-        }
-        handle
-        coverPicture {
-          ... on NftImage {
-            contractAddress
-            tokenId
-            uri
-            verified
-          }
-          ... on MediaSet {
-            original {
-              url
-              mimeType
-            }
-          }
-          __typename
-        }
-        ownedBy
-        dispatcher {
-          address
-          canUseRelay
-        }
-        stats {
-          totalFollowers
-          totalFollowing
-          totalPosts
-          totalComments
-          totalMirrors
-          totalPublications
-          totalCollects
-        }
-        followModule {
-          ... on FeeFollowModuleSettings {
-            type
-            amount {
-              asset {
-                symbol
-                name
-                decimals
-                address
-              }
-              value
-            }
-            recipient
-          }
-          ... on ProfileFollowModuleSettings {
-           type
-          }
-          ... on RevertFollowModuleSettings {
-           type
-          }
-        }
+        recipient
+      }
+      ... on ProfileFollowModuleSettings {
+       type
+      }
+      ... on RevertFollowModuleSettings {
+       type
+      }
     }
-  }`
-
+}
+}
+`
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import { client, recommendedProfiles } from '../api'
+import Link from 'next/link'
+import Image from 'next/image'
 
 
 export default function Home() {
 
+  const [profiles, setProfiles] = useState([])
   useEffect(() => {
     fetchProfile()
   }, [])
@@ -12,12 +15,28 @@ export default function Home() {
     try {
       const response = await client.query(recommendedProfiles).toPromise()
       console.log({response})
+      setProfiles(response.data.recommendedProfiles)
+
     } catch (error) {
       console.log({error})
     }
   }
   return (
     <div>
+      {
+        profiles.map((profile, index) => (
+        <Link href={`/profile/${profile.id}`} key={index}>
+          <a>
+            <div>
+              {
+                profile.picture ? (<Image src={profile.picture.original.url} width="60px" height="60px"/>) : (<p>no pic</p>)
+              }
+              <h2>{profile.handle}</h2>
+              <p>{profile.bio}</p>
+            </div>
+          </a>
+        </Link>
+      ))}
     </div>
   )
 }
