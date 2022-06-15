5b9b7d77f7025f56c84e01425952aee34ebeccec
McDonald Namba <nambamcdonald@gmail.com>
2022-06-15 12:53:01 +0200

    Added connect & follow acc functions

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,12 @@ export const getStaticProps = async () => {
 const Home = ({profiles}) => {
   return (
     <div>
+      <h1 className="flex justify-center">Explore</h1>
       {
         profiles.map(profile => (
         <Link href={'/profile/' + profile.id} key={profile.id}>
           <a>
-            <div className="m-4 p-8 bg-gray-400">
+            <div className="my-4 p-4 bg-gray-400">
 
               <h2 className="text-xl text-red-700">{profile.name}</h2>
               <h2 className="text-sm py-2">{profile.handle}</h2>
--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,5 +1,9 @@
-import { client, getProfiles, recommendedProfiles } from '../../api'
+import { client, getProfiles, getPublications, recommendedProfiles } from '../../api'
 import Image from 'next/image'
+import { ethers } from 'ethers'
+
+import ABI from '../../abi.json'
+const address = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d"
 
 
 export const getStaticPaths = async () => {
@@ -21,27 +25,101 @@ export const getStaticProps = async (context) => {
   const id = context.params.id
   const response = await client.query(getProfiles, { id }).toPromise()
   const data = await response.data.profiles.items[0]
-
+  const publicationsRes = await client.query(getPublications, {id}).toPromise()
+  const publicationsData = await publicationsRes.data.publications.items
   return {
-    props: { profile: data}
+    props: {
+      profile: data,
+      pubs: publicationsData}
   }
 }
 
 
-const Profile = ({profile}) => {
+async function connect() {
+  const account = await window.ethereum.request({
+    method: "eth_requestAccounts"
+  })
+}
+
+
+
+const Profile = ({profile, pubs}) => {
+
+  async function followAcc() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const sign = provider.getSigner()
+    const contract = new ethers.Contract( address, ABI, sign)
+    try {
+      const transaction = contract.follow([profile.id.toString()], [0x0]).toPromise()
+      await transaction.wait()
+      console.log("Account followed")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
-      {
-        profile.picture ? (
-          <Image src={profile.picture.original.url} width="200px" height="200px"/>
-        ) : (<div className="w-40 h-40 bg-black"/>)
-      }
 
-      <p className="text-xl text-red-700 p-2">{profile.name}</p>
+      <div className="flex justify-center">
+        {
+          profile.picture ? (
+            <Image src={profile.picture.original.url} width="200px" height="200px"/>
+          ) : (<div className="w-40 h-40 bg-black"/>)
+        }
+      </div>
+
+      <p className="flex justify-center text-xl text-red-700 my-2">{profile.name}</p>
+
+      <div className="flex justify-evenly m-4">
+        <button onClick={connect} className="flex justify-center items-center w-20 p-1 mb-2 bg-gray-400 text-sm">
+          Connect
+        </button>
+        <button onClick={followAcc} className="flex justify-center items-center w-20 p-1 mb-2 bg-gray-400 text-sm">
+          Follow
+        </button>
+      </div>
+
       <p className="text-sm">{profile.bio}</p>
-      <p>Following - {profile.stats.totalFollowing}</p>
-      <p>Followers - {profile.stats.totalFollowers}</p>
-      <p>Posts - {profile.stats.totalPosts}</p>
+
+      <div className="flex justify-evenly mx-6 my-2">
+        <p>Following - {profile.stats.totalFollowing}</p>
+        <p>Followers - {profile.stats.totalFollowers}</p>
+      </div>
+
+
+      <div className="flex flex-col items-center">
+        <div className="flex justify-evenly w-full">
+          <h2>Posts</h2>
+          <h2>Media</h2>
+          <h2>Collections</h2>
+        </div>
+        {
+          pubs.map(pub => (
+            <div className="flex flex-col bg-gray-400 p-4 my-4 w-full"key={pub.id}>
+              {pub.metadata.content}
+              <div className="flex justify-evenly items-center pt-4">
+
+                <div className="flex items-center">
+                  <p>Mirrors-{pub.stats.totalAmountOfMirrors}</p>
+                  <button className="w-3 h-3 mx-1 bg-black"></button>
+                </div>
+
+                <div className="flex items-center">
+                  <p>Collects-{pub.stats.totalAmountOfCollects}</p>
+                  <button className="w-3 h-3 mx-1 bg-black"></button>
+                </div>
+
+                <div className="flex items-center">
+                  <p>Comments-{pub.stats.totalAmountOfComments}</p>
+                  <button className="w-3 h-3 mx-1 bg-black"></button>
+                </div>
+
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
